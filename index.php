ca093a6d7d7879b579e3c94d560207c7194207a7
I-Am-Zee <nxmbx123@hotmail.com>
2023-01-03 05:02:22 -0600

    init

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html class='page font-Roboto' lang='en'>
+
+    <head>
+        <meta name="description" content="A Mod For Apex Legends." />
+    
+        <meta name="twitter:site" content="r5reloaded.com" />
+        <meta name="twitter:title" content="R5Reloaded" />
+        <meta name="twitter:description" content="A Mod For Apex Legends." />
+        <meta name="twitter:image" content="./content/logo.png" />
+    
+        <meta property='og:title' content='r5reloaded.com' />
+        <meta property='og:type' content="R5Reloaded" />
+        <meta property='og:description' content='A Mod For Apex Legends.' />
+        <meta property="og:image" content='./content/logo.png' />
+        <meta name="theme-color" content="#ca4c4c" />
+    
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='viewport' />
+        <meta charset='utf-8' />
+        <meta name='language' content='english' />
+        <meta name='keywords' content='website' />
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.4/tailwind.min.css' />
+        <link rel='stylesheet' href='./styles/about-me.css' />
+        <link rel='stylesheet' href='./styles/page.css' />
+        <link rel='stylesheet' href='./styles/cards.css' />
+    
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet' />
+    
+        <link rel='preconnect' href='https://fonts.googleapis.com'>
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+        <link href='https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap' rel='stylesheet'>
+    
+    
+        <link rel="shortcut icon" href="./content/logo.png" type="image/x-icon">
+        <title>R5Reloaded</title>
+    </head>
+    
+    <body style='padding: 0rem;'>
+        <div class='top-padding'>
+            <div class='pfp-flex'>
+                <div class='md:w-1/2 w-screen text-margin'>
+                    <text class='name'>Welcome to R5Reloaded</text>
+                    <p class='sub-text'>
+                        <br /><br />
+                        <bold class='hight-light-text-gry'>!</bold> Visit our <a class='saho' href='https://github.com/Mauler125/r5sdk'>Github</a>. <br>
+                        <bold class='hight-light-text-gry'>!</bold> Join our <a class='saho' href='https://discord.gg/r5reloaded'>Discord</a>. <br>
+                        <bold class='hight-light-text-gry'>!</bold> Go to our <a class='saho' href='https://twitter.com/r5reloaded'>Twitter</a>. <br>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="list">
+            <a>
+                <div class='card' onclick='window.open("https://docs.r5reloaded.com/")'>
+                    <div style='display: flex;'>
+                        <img src='./content/logo.png' class='card-img' alt='Twitter' draggable=false>
+                        <span class='card-title'>Documentation/Wiki</span>
+                    </div>
+                    <p class='card-desc'>
+                        You can find useful tutorials and information on our wiki page.
+                    </p>
+                    <p class='card-url'>
+                        https://docs.r5reloaded.com/
+                    </p>
+                </div>
+            </a>
+            <a>
+                <div class='card'>
+                    <div style='display: flex;'>
+                        <img src='./content/logo.png' class='card-img' alt='Twitter' draggable=false>
+                        <span class='card-title'>Server List</span>
+                    </div>
+                    <p class='card-desc'>
+                        Want to see what servers are currently operational and running?
+                    </p>
+                    <p class='card-url'>
+                        Not Done
+                    </p>
+                </div>
+            </a>
+            <a>
+                <div class='card' onclick='window.open("https://trello.com/b/ymr4R3j9/r5reloaded")'>
+                    <div style='display: flex;'>
+                        <img src='./content/logo.png' class='card-img' alt='Twitter' draggable=false>
+                        <span class='card-title'>Trello</span>
+                    </div>
+                    <p class='card-desc'>
+                        Want to see what bugs need fixed, or just view overall progress?
+                    </p>
+                    <p class='card-url'>
+                        https://trello.com/b/ymr4R3j9/r5reloaded
+                    </p>
+                </div>
+            </a>
+            <a>
+                <div class='card' onclick='window.open("https://github.com/Mauler125/r5sdk/releases")'>
+                    <div style='display: flex;'>
+                        <img src='./content/logo.png' class='card-img' alt='Twitter' draggable=false>
+                        <span class='card-title'>Download the Mod</span>
+                    </div>
+                    <p class='card-desc'>
+                        If you need any help downloading or installing the mod join our discord!
+                    </p>
+                    <p class='card-url'>
+                        https://github.com/Mauler125/r5sdk/releases
+                    </p>
+                </div>
+            </a>
+            <br>
+        </div>
+    </body>
+</html>
